2ec0109a82fde732c8a2268a73e30abefae2a993
ChebbiYahya <ghofran.bentk@gmail.com>
2024-07-13 11:24:58 +0100

    Constat CRUD

--- routes/constatRoutes.js
+++ routes/constatRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const constatController = require('../controllers/constatController');
+const { verifyToken } = require('../middleware/authMiddleware');
 
-router.post('/', constatController.createConstat);
-router.get('/', constatController.getAllConstats);
-router.get('/:id', constatController.getConstatById);
-router.put('/:id', constatController.updateConstat);
-router.delete('/:id', constatController.deleteConstat);
+router.post('/',verifyToken, constatController.createConstat);
+router.get('/', verifyToken,constatController.getAllConstats);
+router.get('/:id',verifyToken, constatController.getConstatById);
+router.put('/:id',verifyToken,  constatController.updateConstat);
+router.delete('/:id',verifyToken,  constatController.deleteConstat);
 
 module.exports = router;
