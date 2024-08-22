95e195a460a2cb66154460df21efde4da5ac90b5
ChebbiYahya <ghofran.bentk@gmail.com>
2024-08-22 17:09:55 +0100

    roles

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const constatController = require('../controllers/constatController');
 const pdfController = require('../controllers/pdfController');
 const { verifyToken, isAdmin } = require('../middleware/authMiddleware');
+const { verifyToken, isAdmin, isSuperAdmin } = require('../middleware/authMiddleware');
 
-router.post('/generate',  pdfController.createConstat);
+router.post('/generate',[verifyToken, isAdmin] , pdfController.createConstat);
 
 
 
